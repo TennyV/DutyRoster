d267ec46bbc452388f53da89e295e4c99d7545b5
Siriwat Vongtip <siriwat.vongtip@snhu.edu>
2022-10-01 14:34:34 -0400

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+    private String lastName;
+    private String firstName;
+    private String rank;
+    private String data;
+    private String assignedDuty;
+    private String previousDutyAssigned;
+}
---
+++ src/main/java/Soldier.java
@@ -0,0 +1,8 @@
+public class Soldier {
+
+    private String lastName;
+    private String firstName;
+    private String rank;
+    private String assignedDuty;
+    private String previousDutyAssigned;
+}
